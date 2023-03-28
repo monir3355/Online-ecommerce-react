396115bd2e6b4fb242f2981e9a76e2296f11cbf4
monir3355 <16173103036@cse.bubt.edu.bd>
2023-03-29 04:31:44 +0600

    Online Ecommerce with react

---
+++ src/common/header/Head.jsx
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Head = () => {
+  return (
+    <>
+      <section className='head'>
+        <div className='container d_flex'>
+          <div className='left row'>
+            <i className='fa fa-phone'></i>
+            <label> +8801737368680</label>
+            <i className='fa fa-envelope'></i>
+            <label> dev1.monirul@gmail.com</label>
+          </div>
+          <div className='right row RText'>
+            <label>Theme FAQ"s</label>
+            <label>Need Help?</label>
+            <label>EN</label>
+            <label>USD</label>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Head
